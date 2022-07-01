4a432addc5ff9193f4acff3f7eca76a0ac404941
SN-Grotesque <z7z-h@outlook.com>
2022-07-01 12:06:31 +0800

    cipher

--- s2048.c
+++ s2048.c
@@ -33,50 +33,35 @@ static uint8_t *file_read(FILE *stream)
 
 int main(int argc, char **argv)
 {
-    if(argc != 5) {
-        printf("./main [输入文件路径] [输出文件路径] [密码] [e/d]\n");
-        return EOF;
-    }
-    if (strcmp(argv[1], argv[2]) == 0) {
-        printf("请不要使用同一文件作为输入与输出文件\n");
-        return EOF;
-    }
-
     sn *data = (sn *)malloc(sizeof(sn));
-    char *in_fileName = (char *)malloc(256);
-    char *out_fileName = (char *)malloc(256);
-    data->u_key = (u8 *)malloc(256);
 
-    memcpy(in_fileName, argv[1], strlen(argv[1]));
-    memcpy(out_fileName, argv[2], strlen(argv[2]));
-    memcpy(data->u_key, argv[3], strlen(argv[3]));
+    char temp[4096] = {
+        "0000000000000000000000000000000000000000000000000000000000000000"
+        "0000000000000000000000000000000000000000000000000000000000000000"
+        "0000000000000000000000000000000000000000000000000000000000000000"
+        "0000000000000000000000000000000000000000000000000000000000000000"
+    };
+    char key_temp[257] = {
+        "----------------------------------------------------------------"
+        "----------------------------------------------------------------"
+        "----------------------------------------------------------------"
+        "----------------------------------------------------------------"
+    };
+    data->data = (u8 *)temp;
+    data->len = strlen(temp);
+    data->token = (u8 *)key_temp;
 
-    FILE *file_ip = fopen(in_fileName, "rb");
-    if (!file_ip) {printf("文件打开失败\n"); return -1;}
-    data->data = file_read(file_ip);
-    data->data_n = get_file_size(file_ip);
-    fclose(file_ip);
+    S2048_Padding(data);
+    S2048_ENCRYPT(data);
 
-    SN_INIT_KEY(data);
-
-    if (argv[4][0] == 'e') {
-        SN_INIT_DATA(data);
-        printf("开始加密...\n");
-        ENCRYPT_DATA(data);
-        printf("加密完成.\n");
-    } else if (argv[4][0] == 'd') {
-        DECRYPT_DATA(data);
-    } else {
-        printf("请确认是'加密'/'解密'.\n");
-        return EOF;
+    for(int x = 0;x < data->len; ++x) {
+        printf("%02X", data->data[x]);
+        if((x+1) % 16 == 0) {
+            printf("\n");
+        } else {
+            printf(" ");
+        }
     }
 
-    FILE *file_op = fopen(out_fileName, "wb");
-    for(u64 x = 0; x < data->data_n; ++x) {
-        fputc(data->data[x], file_op);
-    }
-    fclose(file_op);
-
-    free(data);
     return 0;
 }

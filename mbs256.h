b15266036ccefcca3ce4f2b53c17b6fff87f0831
SN-Grotesque <z7z-h@outlook.com>
2022-10-27 14:01:11 +0800

    cryptographic

--- mbs256.h
+++ mbs256.h
@@ -5,10 +5,6 @@
 #include <stdint.h>
 
 #define MBS256_BlockSize 32
-
-#define MBS256_Key_MIN 0x1000
-#define MBS256_Key_MAX 0xff00
-
 #define MBS256_Round 3
 
 #define _ENCRYPT(m, k) (uint16_t)((m << 4) ^ k)
@@ -19,35 +15,26 @@ typedef struct {
     size_t size;
     uint16_t *key;
     uint16_t *result;
-} mbs256;
+} mbs256_ctx;
 
-static void setRandomSeed()
-{
-    clockid_t a = 0;
-    struct timespec p;
-    clock_gettime(a, &p);
-    srand((unsigned)p.tv_nsec);
-}
+static const uint8_t sbox[4] = {
+    0x9a, 0x2f, 0x18, 0xcd
+};
 
-static uint32_t randint(uint32_t min, uint32_t max)
+static int mbs256_encrypt(mbs256_ctx *ctx)
 {
-    return rand() % (max - min + 1) + min;
-}
+    uint8_t __buff[5];
+    for(int x = 0; x < ctx->size; ++x) {
+        sprintf((char *)__buff, "%x", _ENCRYPT(ctx->message[x], ctx->key[x]));
+        __buff[0] ^= sbox[0];
+        __buff[1] ^= sbox[1];
+        __buff[2] ^= sbox[2];
+        __buff[3] ^= sbox[3];
+        // 将__buff转整型，再与key做异或操作
 
-static uint16_t *mbs256_gen_key()
-{
-    setRandomSeed();
-    uint16_t *_key = (uint16_t *)malloc(sizeof(uint16_t) * MBS256_BlockSize);
-    for(uint8_t keyIndex = 0; keyIndex < MBS256_BlockSize; ++keyIndex) {
-        _key[keyIndex] = randint(MBS256_Key_MIN, MBS256_Key_MAX);
-    }
-    return _key;
-}
-
-static int mbs256_encrypt(mbs256 *ctx)
-{
-    for(size_t x = 0; x < ctx->size; ++x) {
-        ctx->result;
+        // for(int _ = 0; _ < 4; ++_) {
+        //     printf("%02x ", __buff[_]);
+        // } printf("\n");
     }
 
     return 0;

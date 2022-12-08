2e110a903630217885b69348a130695c3128b92b
SN-Grotesque <z7z-h@outlook.com>
2022-12-09 01:44:53 +0800

    cryptographic

--- s2048/s2048.h
+++ s2048/s2048.h
@@ -6,17 +6,20 @@
 
 #define S2048_BlockSize 256
 #define S2048_Rounds 9
-#define S2048_E(m, k) ((((m-k) ^ ~(k+78)) - (((k+17) & 0xff) >> 1)) & 0xff)
+#define S2048_Maximum 16
+
+#define S2048_E(m, k) ((((m - k) ^ ~(k+78)) - (((k+17) & 0xff) >> 1)) & 0xff)
 #define S2048_D(c, k) ((((c + (((k+17) & 0xff) >> 1)) ^ ~(k+78)) + k) & 0xff)
+#define S2048_RANGE(index) (((index - 7) % S2048_BlockSize) & 0xff)
+#define S2048_INIT_RK(buf, iv, i, r) \
+    S2048_D(S2048_D(buf, iv), S2048_D(buf ^ (iv - i), (i + r + (iv ^ buf))))
+
+typedef uint8_t s2048_state_t[S2048_Maximum][S2048_Maximum];
 
 typedef struct {
-    uint8_t *data;
-    uint8_t key[S2048_BlockSize];
-    uint8_t keySet[S2048_Rounds][S2048_BlockSize];
-    size_t  size;
+    uint8_t IV[S2048_BlockSize];
+    uint8_t RK[S2048_Rounds][S2048_BlockSize];
 } s2048_ctx;
 
-void s2048_roundKey(s2048_ctx *ctx);
-void s2048_encrypt(s2048_ctx *ctx);
-void s2048_decrypt(s2048_ctx *ctx);
+void s2048_init_ctx(s2048_ctx *ctx, uint8_t *key);
 #endif // S2048
--- snCrypto.h
+++ snCrypto.h
@@ -4,6 +4,8 @@
 #include "../swap.h"
 #include "../random/random.h"
 
+#define SBOX_BLOCKLEN 256
+
 static void elementShift(uint8_t *data, size_t size)
 {
     for(size_t i = 0; i < size / 2; ++i) {
@@ -11,26 +13,26 @@ static void elementShift(uint8_t *data, size_t size)
     }
 }
 
-static void sbox_init(uint8_t *sbox, uint16_t size)
+static void sbox_init(uint8_t *sbox)
 {
     setRandomSeed();
     uint16_t r, x, left, right;
-    for(x = 0; x < size; ++x)
+    for(x = 0; x < SBOX_BLOCKLEN; ++x)
         sbox[x] = x;
     for(r = 0; r < 16; ++r) {
-        for(x = 0; x < size; ++x) {
+        for(x = 0; x < SBOX_BLOCKLEN; ++x) {
             do {
-                left  = randint(0, size - 1);
-                right = randint(0, size - 1);
+                left  = randint(0, SBOX_BLOCKLEN - 1);
+                right = randint(0, SBOX_BLOCKLEN - 1);
             } while(left == right);
             swap_uint8(&sbox[left], &sbox[right]);
         }
     }
 }
 
-static void rsbox_init(uint8_t *sbox, uint8_t *rsbox, uint16_t size)
+static void rsbox_init(uint8_t *sbox, uint8_t *rsbox)
 {
-    for(uint16_t x = 0; x < size; ++x)
+    for(uint16_t x = 0; x < SBOX_BLOCKLEN; ++x)
         rsbox[sbox[x]] = x;
 }
 
